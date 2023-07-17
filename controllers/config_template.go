d51dd221fdbde9094e00b426ecd4c04ed5c49e13
DiptoChakrabarty <diptochuck123@gmail.com>
2023-07-17 17:30:31 +0530

    use envFrom instead of env for statefulset

--- controllers/config_template.go
+++ controllers/config_template.go
@@ -6,7 +6,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func NewMySQLStatefulSet(name, namespace, passwordSecretName string, replicas int32) *appsv1.StatefulSet {
+func NewMySQLStatefulSet(name string, namespace string, SecretName string, replicas int32) *appsv1.StatefulSet {
 	statefulSet := &appsv1.StatefulSet{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
@@ -30,15 +30,11 @@ func NewMySQLStatefulSet(name, namespace, passwordSecretName string, replicas in
 						{
 							Name:  "mysql",
 							Image: "mysql:8.0.26", // Replace with your MySQL image
-							Env: []corev1.EnvVar{
+							EnvFrom: []corev1.EnvFromSource{
 								{
-									Name: "MYSQL_ROOT_PASSWORD",
-									ValueFrom: &corev1.EnvVarSource{
-										SecretKeyRef: &corev1.SecretKeySelector{
-											LocalObjectReference: corev1.LocalObjectReference{
-												Name: passwordSecretName,
-											},
-											Key: "password",
+									SecretRef: &corev1.SecretEnvSource{
+										LocalObjectReference: corev1.LocalObjectReference{
+											Name: SecretName,
 										},
 									},
 								},
@@ -53,14 +49,16 @@ func NewMySQLStatefulSet(name, namespace, passwordSecretName string, replicas in
 	return statefulSet
 }
 
-func NewMySQLSecret(name, namespace, password string) *corev1.Secret {
+func NewMySQLSecret(name string, namespace string, rootPwd string, clusteradminPwd string) *corev1.Secret {
 	secret := &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
 		},
 		Data: map[string][]byte{
-			"password": []byte(password),
+			"MYSQL_ROOT_PASSWORD": []byte(rootPwd),
+			"MYSQL_USER":          []byte("clusteradmin"),
+			"MYSQL_PASSWORD":      []byte(clusteradminPwd),
 		},
 	}
 
