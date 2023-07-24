02033f7d58790b2b6d16f21ef6e4e13de30c46b3
DiptoChakrabarty <diptochuck123@gmail.com>
2023-07-24 21:54:45 +0530

    kubebuilder rbac permissions addition

--- controllers/mysqlinstance_controller.go
+++ controllers/mysqlinstance_controller.go
@@ -40,6 +40,8 @@ type MySQLInstanceReconciler struct {
 //+kubebuilder:rbac:groups=dipto.mysql.example.dipto.mysql.example,resources=mysqlinstances,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=dipto.mysql.example.dipto.mysql.example,resources=mysqlinstances/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=dipto.mysql.example.dipto.mysql.example,resources=mysqlinstances/finalizers,verbs=update
+// +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
